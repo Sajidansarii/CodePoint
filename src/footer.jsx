164a072b853e6ecd890b1cff50abb59e8bdc624c
Sajid <sajidabcfiz5678910@gmail.com>
2026-01-12 14:35:55 +0530

    Upgrade

--- src/footer.jsx
+++ src/footer.jsx
@@ -38,7 +38,7 @@ function Footer() {
           <div className="mt-10  space-x-4 justify-center md:justify-center">
           <h3 className="text-2xl font-medium mb-4">Follow Us</h3>
           <a className="px-2 py-1 text-2xl rounded-full hover:bg-green-50 transition-all duration-200 " target="blank" href="https://www.linkedin.com/feed/"><i class="fa-brands fa-linkedin"></i></a>
-          <a className="px-2 py-1 text-2xl rounded-full hover:bg-green-50 transition-all duration-200" target="blank" href="https://x.com/home"><i class="fa-brands fa-x"></i></a>
+          <a className="px-2 py-1 text-2xl rounded-full hover:bg-green-50 transition-all duration-200" target="blank" href="https://x.com/sajid564125"><i class="fa-brands fa-x"></i></a>
           <a className="px-2 py-1 text-2xl rounded-full hover:bg-green-50 transition-all duration-200" target="blank" href="https://www.instagram.com/sa_jid6223/"><i class="fa-brands fa-instagram"></i></a>
           <a className="px-2 py-1 text-2xl rounded-full hover:bg-green-50 transition-all duration-200" target="blank" href="https://github.com/"><i class="fa-brands fa-github"></i></a>
           </div>
